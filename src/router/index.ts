af0c7cc4fea6b73d484ca4acbeb4719a398830f3
cloudblaze123 <cloudblaze123@gmail.com>
2024-08-20 15:39:06 +0800

    主要变化：调整了路由，将 Home 页面作为根路由；Home 页面添加导航栏和一级页面，可以切换联系人、聊天室列表

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,12 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '@/components/Login.vue'
 import Home from '@/views/Home.vue'
+import Login from '@/components/Login.vue'
 import Chat from '@/components/Chat.vue'
-import ContactList from '@/components/ContactList.vue'
 
 const routes = [
     {
         path: '/',
-        name: 'Login',
-        component: Login
-    },
-    {
-        path: '/Home',
         name: 'Home',
         component: Home,
         children: [
@@ -22,7 +16,12 @@ const routes = [
                 component: Chat
             },
         ]
-    }
+    },
+    {
+        path: '/Login',
+        name: 'Login',
+        component: Login
+    },
 ]
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,37 @@
 <template>
     <h1 class="bg-blue-500 text-white text-center text-3xl p-3 font-bold">Simple Chat</h1>
     <div class="h-screen w-full bg-gray-100 flex">
-        <ContactList class="h-full w-1/4 flex-grow" />
+        <!-- 导航栏 -->
+        <div class="w-1/8 bg-gray-200 flex flex-col items-center space-y-3">
+            <div 
+                class="flex justify-center items-center bg-white rounded-lg p-3 cursor-pointer" 
+                @click="pos = 'contact-list'">
+                联系人
+            </div>
+            <div 
+                class="flex justify-center items-center bg-white rounded-lg p-3 cursor-pointer" 
+                @click="pos = 'chat-room'">
+                聊天室
+            </div>
+        </div>
+        
+        <!-- 一级页面 -->
+        <div class="flex justify-center items-center w-1/4 bg-gray-200">
+            <div 
+                class="w-full h-full rounded-lg"
+                v-show="pos === 'contact-list'">
+                <ContactList class="h-full" />
+            </div>
+            <div 
+                class="w-full h-full rounded-lg"
+                v-show="pos === 'chat-room'">
+                <div class="w-full h-full p-8 bg-white rounded-lg  shadow-lg">
+                    <h1 class="text-2xl font-bold text-center mb-6">聊天室列表</h1>
+                </div>
+            </div>
+        </div>
+        
+        <!-- 二级页面 -->
         <router-view class="h-full w-3/4 flex-grow hidden sm:block"></router-view>
     </div>
 </template>
@@ -9,6 +39,10 @@
 
 
 <script setup lang="ts">
+import { ref } from "vue";
 import ContactList from "@/components/ContactList.vue";
+
+const pos = ref('');
+
 
 </script>
