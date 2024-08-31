0f86a89b759f1e8b6d2d6baded85b715e675e621
cloudblaze123 <cloudblaze123@gmail.com>
2024-08-31 17:32:01 +0800

    小更新：重构代码，聊天室 Chat 改为通过 UserStore 的 getUser 方法获取用户信息

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -8,9 +8,10 @@
 
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watch, Ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { useAuthStore } from '@/stores/auth';
+import { useUserStore } from '@/stores/user'
 import { useMessageStore } from '@/stores/message';
 import { useSessionStore } from '@/stores/session';
 
@@ -19,23 +20,32 @@ import MessageDisplay from '@/components/chat/MessageDisplay.vue';
 import MessageInput from '@/components/chat/MessageInput.vue';
 
 import { getMessages } from '@/api/message';
-import { getUser } from '@/api/user';
+
+import { User } from '@/models/User'
 import { Message } from '@/models/Message';
 
 
 const route = useRoute();
 const authStore = useAuthStore();
+const userStore = useUserStore();
 const messageStore = useMessageStore();
 const sessionStore = useSessionStore();
 
-let to = ref(null);
-to.value = getUser(route.params.id as string);
+let to:Ref<User|{id:string}> = ref({
+    id: '-1',
+})
+updateTo()
 
 watch(route, () => {
     console.log('route changed');
     fetchMessages();
-    to.value = getUser(route.params.id as string);
+    updateTo()
 });
+
+function updateTo(){
+    userStore.getUser(route.params.id as string)
+        .then(user => to.value = user)
+}
 
 fetchMessages();
 
