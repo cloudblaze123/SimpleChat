699e13431ec87fdf42b2c8df61485c65d9fe71a2
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-13 19:35:16 +0800

    小更新：优化【用户资料页】的UI

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,7 +4,7 @@
         <div class="flex justify-between items-center p-4 bg-white dark:bg-slate-900">
             <div class="relative w-full flex justify-center items-center">
                 <!-- 返回按钮 -->
-                <button 
+                <button
                     class="absolute left-0 top-0 text-gray-400 hover:text-gray-600 dark:hover:text-gray-300 transition-all duration-400 ease-in-out lg:hidden"
                     @click="goBack">
                     <Icon size="32">
@@ -12,33 +12,66 @@
                     </Icon>
                 </button>
                 <h1 class="text-2xl text-center">
-                    这是 {{ user ? user.name:'未知用户' }} 的Profile页面
+                    Profile页面
                 </h1>
             </div>
         </div>
 
         <!-- Profile页面的主体 -->
-        <div class="flex flex-col items-center h-full w-full rounded-lg overflow-y-auto">
-            <div class="flex flex-col items-center p-4">
-                <!-- 用户头像 -->
-                <div class="flex items-center justify-center bg-blue-500 text-white text-3xl font-bold rounded-full overflow-hidden w-32 h-32">
-                    {{ user ? user.name[0] : '' }}
+        <div class="flex flex-col justify-between items-center h-full w-full p-4 rounded-lg overflow-y-auto">
+
+            <div v-if="!user">
+                未知用户
+            </div>
+            <!-- 用户信息 -->
+            <div v-else class="flex flex-col">
+                <!-- 头部信息 -->
+                <div class="flex items-center space-x-2">
+                    <!-- 用户头像 -->
+                    <div
+                        class="flex items-center justify-center bg-blue-500 text-white text-3xl font-bold rounded-full overflow-hidden w-24 h-24">
+                        {{ user.name[0] }}
+                    </div>
+
+                    <!-- 用户名称 -->
+                    <h2 class="text-lg font-bold text-gray-900 dark:text-gray-100">
+                        {{ user.name }}
+                    </h2>
                 </div>
-                <!-- 用户信息 -->
-                <div class="flex flex-col items-center mt-4 space-y-2">
-                    <h2 class="text-lg font-bold text-gray-900 dark:text-gray-100">{{ user ? user.name : '未知用户' }}</h2>
-                    <p class="text-sm text-gray-600 dark:text-gray-300">{{ user ? user.email : '未知邮箱' }}</p>
-                    <p class="text-sm text-gray-600 dark:text-gray-300">{{ user ? (user.signature ? user.signature : '未填写签名') : '未知签名' }}</p>
-                    <p class="text-sm text-gray-600 dark:text-gray-300">{{ user ? (user.gender ? user.gender : '未填写性别') : '未知性别' }}</p>
-                    <p class="text-sm text-gray-600 dark:text-gray-300">{{ user ? (user.age ? user.age : '未填写年龄') : '未知年龄' }}</p>
-                    <p class="text-sm text-gray-600 dark:text-gray-300">{{ user ? (user.birthday ? user.birthday : '未填写生日') : '未知生日' }}</p>
-                    <p class="text-sm text-gray-600 dark:text-gray-300">{{ user ? (user.location ? user.location : '未填写地区') : '未知地区' }}</p>
-                    
+
+                <div class="flex flex-col mt-4 space-y-2">
+                    <!-- 基本信息 -->
+                    <div class="flex items-center space-x-2">
+                        <p v-if="user.gender" class="text-sm text-gray-600 dark:text-gray-300">
+                            {{ user.gender }}
+                        </p>
+
+                        <p v-if="user.age" class="text-sm text-gray-600 dark:text-gray-300">
+                            {{ `${user.age}岁` }}
+                        </p>
+
+                        <p v-if="user.birthday" class="text-sm text-gray-600 dark:text-gray-300">
+                            {{ user.birthday }}
+                        </p>
+
+                        <p v-if="user.location" class="text-sm text-gray-600 dark:text-gray-300">
+                            {{ user.location }}
+                        </p>
+
+                    </div>
+
+                    <p v-if="user.email" class="text-sm text-gray-600 dark:text-gray-300">
+                        {{ user.email }}
+                    </p>
+
+                    <p v-if="user.signature" class="max-w-96 text-sm text-gray-600 dark:text-gray-300">
+                        {{ user.signature }}
+                    </p>
+
                 </div>
             </div>
-            <button class="p-4 bg-blue-700 rounded-md text-white"
-                @click="gotoChat"
-                v-if="id">
+
+            <button class="p-4 bg-blue-700 rounded-md text-white" @click="gotoChat" v-if="id">
                 发消息
             </button>
         </div>
@@ -47,13 +80,12 @@
 
 
 <script setup lang="ts">
-import { ref,computed, watch } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 
 import { Icon } from "@vicons/utils";
 import { ChevronLeft } from "@vicons/tabler";
 
-import { User } from "@/models/User";
 import { useAuthStore } from '@/stores/auth';
 import { useUserStore } from '@/stores/user'
 
@@ -81,7 +113,7 @@ function goBack() {
     router.push({ name: 'Home' });
 }
 
-function gotoChat(){
+function gotoChat() {
     router.push({ name: 'ChatTo', params: { id: id.value } });
 }
 </script>
