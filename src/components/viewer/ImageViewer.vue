8d9b0e743c2808a018617629e814ad97a46a46bf
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-21 15:09:38 +0800

    主要变化：重写【图片查看器】组件的拖动与缩放的输入处理，支持触控设备通过两点缩放手势放大或缩小图片

--- src/components/viewer/ImageViewer.vue
+++ src/components/viewer/ImageViewer.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- 注释内容来源于网络：当想要通过触控拖动元素时，需要把该元素 touch-action 设为 none，不然 pointermove 事件触发几次后就会自动取消 -->
-    <div ref="container" @wheel.prevent="handleWheel" @pointerdown.prevent="handleDragStart" class="flex h-0 w-full grow justify-center items-center overscroll-none cursor-grab touch-none">
+    <div ref="container" @wheel.prevent="handleWheel" @pointerdown.prevent="handlePointerDown" class="flex h-0 w-full grow justify-center items-center overscroll-none cursor-grab touch-none">
         <img ref="image" :src="props.src" alt="" class="w-full h-full object-contain select-none">
     </div>
 </template>
@@ -32,7 +32,7 @@ let zoomLevel = 1;
 
 
 function handleWheel(event: WheelEvent) {
-    if (event.deltaY <    0) {
+    if (event.deltaY < 0) {
         // 放大
         zoomLevel *= 1.1;
     } else {
@@ -45,43 +45,181 @@ function handleWheel(event: WheelEvent) {
 
 
 
+const pointers: Array<PointerEvent> = [];
+
 
 
 let isDragging = false;
+let isPinching = false; // 是否正在进行捏合手势
 
-let translateX:number = 0;
-let translateY:number = 0;
+let translateX: number = 0;
+let translateY: number = 0;
 
-let lastX:number = 0;
-let lastY:number = 0;
+let lastX: number = 0;
+let lastY: number = 0;
 
 
-function handleDragStart(event: PointerEvent) {
-    container.value.setPointerCapture(event.pointerId);
+let initPinchDistance = 0;
+let initZoomLevel = 1;
+
+
+function handlePointerDown(event: PointerEvent) {
+    // container.value.setPointerCapture(event.pointerId);
+
+    if (pointers.length === 0) {
+        // 初始化
+        console.log('init pointer event listener')
+        isDragging = false;
+        isPinching = false;
+        document.addEventListener('pointerup', handlePointerUp);
+        document.addEventListener('pointermove', handlePointerMove);
+    }
+
+
+    pointers.push(event);
+    console.log('pointerdown')
+    console.log('pointerId:', event.pointerId)
+    console.log('pointers.length:', pointers.length)
+
+
+    if (pointers.length > 1) {
+
+        // 处理捏合手势
+        if (isDragging) {
+            exitDrag()
+        }
+        enterPinch()
+
+    } else {
+        // 处理拖动手势
+        enterDrag()
+    }
+}
+
+
+
+
+function enterDrag() {
+    console.log('start drag')
     isDragging = true;
+    const event = pointers[0];
     lastX = event.clientX;
     lastY = event.clientY;
-    document.addEventListener('pointerup', handleDragEnd);
-    document.addEventListener('pointermove', handleDragMove);
 }
 
-
-function handleDragEnd(event: PointerEvent) {
+function exitDrag() {
+    console.log('end drag')
     isDragging = false;
-    document.removeEventListener('pointermove', handleDragMove);
-    document.addEventListener('pointerup', handleDragEnd);
 }
 
 
-function handleDragMove(event: PointerEvent) {
-    if (isDragging) {
-        const deltaX = event.clientX - lastX;
-        const deltaY = event.clientY - lastY;
-        lastX = event.clientX;
-        lastY = event.clientY;
-        translateX = translateX+deltaX;
-        translateY = translateY+deltaY;
-        image.value.style.translate = `${translateX}px ${translateY}px`;
+function enterPinch() {
+    console.log('start pinch');
+    isPinching = true;
+    initPinchDistance = calculateDistance(pointers[0], pointers[1]);
+    console.log('initPinchDistance:', initPinchDistance)
+    initZoomLevel = zoomLevel;
+    console.log('initZoomLevel:', initZoomLevel)
+}
+
+function exitPinch() {
+    console.log('end pinch');
+    isPinching = false;
+}
+
+
+
+
+function handlePointerUp(event: PointerEvent) {
+    let pointerIndex = -1;
+    // 获取指针对应的索引
+    for (let i = 0; i < pointers.length; i++) {
+        if (pointers[i].pointerId === event.pointerId) {
+            pointerIndex = i;
+            break;
+        }
     }
+
+    if (pointerIndex === -1) {
+        console.log('unknown pointerId:', event.pointerId)
+        return;
+    }
+
+    // 移除指针
+    pointers.splice(pointerIndex, 1);
+    console.log('pointerup')
+    console.log('pointers index removed:', pointerIndex)
+    console.log('pointers.length:', pointers.length)
+    if (pointers.length === 1 && isPinching) {
+        exitPinch()
+        enterDrag()
+    }
+    if (pointers.length === 0) {
+        document.removeEventListener('pointerup', handlePointerUp);
+        document.removeEventListener('pointermove', handlePointerMove);
+
+        // if (isPinching) {
+        //     console.log('end pinch');
+        // } else if (isDragging) {
+        //     console.log('end drag');
+        // }
+    }
+}
+
+
+function handlePointerMove(event: PointerEvent) {
+    // 更新指针
+    for (let i = 0; i < pointers.length; i++) {
+        if (pointers[i].pointerId === event.pointerId) {
+            pointers[i] = event;
+            break;
+        }
+    }
+
+    // 处理手势
+    if (isPinching) {
+        handlePinchMove();
+    } else if (isDragging) {
+        handleDragMove();
+    } else {
+        console.log('unknown pointer input');
+    }
+}
+
+
+
+
+function handleDragMove() {
+    // console.log('drag move')
+    const event = pointers[0];
+
+    const deltaX = event.clientX - lastX;
+    const deltaY = event.clientY - lastY;
+    lastX = event.clientX;
+    lastY = event.clientY;
+    translateX = translateX + deltaX;
+    translateY = translateY + deltaY;
+    image.value.style.translate = `${translateX}px ${translateY}px`;
+}
+
+
+
+
+function handlePinchMove() {
+    // console.log('pinch move')
+    const currentDistance = calculateDistance(pointers[0], pointers[1]);
+    // console.log('currentDistance:', currentDistance)
+    // console.log('initPinchDistance:', initPinchDistance)
+    zoomLevel = initZoomLevel * (currentDistance / initPinchDistance);
+    // console.log('zoomLevel:', zoomLevel)
+    image.value.style.transform = `scale(${zoomLevel})`;
+}
+
+
+function calculateDistance(event1: PointerEvent, event2: PointerEvent) {
+    // console.log('calculate:', pointers[0], pointers[1])
+    const dx = event1.clientX - event2.clientX;
+    const dy = event1.clientY - event2.clientY;
+    return Math.sqrt(dx * dx + dy * dy);
 }
 </script>
