774d563f9ebd78f0469bb9fbb94608f0b3d08028
cloudblaze123 <cloudblaze123@gmail.com>
2025-05-21 21:12:10 +0800

    小更新：修复【图片查看器】组件的图片能阻止组件下方按钮按下的问题

--- src/components/viewer/ImageViewer.vue
+++ src/components/viewer/ImageViewer.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- 注释内容来源于网络：当想要通过触控拖动元素时，需要把该元素 touch-action 设为 none，不然 pointermove 事件触发几次后就会自动取消 -->
     <div ref="container" @wheel.prevent="handleWheel" @pointerdown.prevent="handlePointerDown" class="flex h-0 w-full grow justify-center items-center overscroll-none cursor-grab touch-none">
-        <img ref="image" :src="props.src" alt="" class="w-full h-full object-contain select-none">
+        <img ref="image" :src="props.src" alt="" class="w-full h-full object-contain select-none pointer-events-none">
     </div>
 </template>
 
