05351b1f4df63982009978e9cd42854b383fafdf
cloudblaze123 <cloudblaze123@gmail.com>
2025-06-09 10:31:16 +0800

    小更新：回调视频通话元素尺寸和位置，添加移动端响应式

--- src/components/chat/VideoCall.vue
+++ src/components/chat/VideoCall.vue
@@ -7,14 +7,14 @@
 
             <!-- Content -->
             <div class="flex flex-col flex-1 justify-center items-center">
-                <div class="flex relative">
-                    <div class="w-full h-full">
-                        <!-- remote -->
+                <div class="flex flex-col lg:flex-row">
+                    <div class="lg:w-1/2 m-4">
+                        remote
                         <video ref="remoteVideo" autoplay></video>
                     </div>
-                    <div class="absolute top-0 right-0 bottom-2/3 left-0">
-                        <!-- local -->
-                        <video ref="localVideo" autoplay muted class="h-48 w-48"></video>
+                    <div class="lg:w-1/2 m-4">
+                        local
+                        <video ref="localVideo" autoplay muted></video>
                     </div>
                 </div>
 
