c37aac9ae3ff48d72c780b349baeb5f03875c12a
cloudblaze123 <cloudblaze123@gmail.com>
2025-08-21 00:48:56 +0800

    小更新：DaisyUI主版本由v5降级为v4，从而修复此前项目一直使用错误版本DaisyUI的问题（此前一直使用的是v4版的配置和文档，但安装的却是v5版本）

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,11 @@ export default {
   // daisyUI config (optional - here are the default values)
   // 目前仅使用无样式的（骨架）版本的 daisyUI （不应用 daisyUI 的默认样式）
   daisyui: {  
-    // 将 themes 设为 [] 从而避免引入 daysiUI 主题
+    // 将 themes 设为 [] 可以避免引入 daysiUI 主题
     // （不用 false 是因为使用 false 时依然会引入 daisyUI 的 light 和 dark 主题，会导致夜间主题下应用的字体颜色发生变化）
-    themes:[], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    base: false, // applies background color and foreground color for root element by default
-    styled: false, // include daisyUI colors and design decisions for all components
+    themes: ['light', 'dark'], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
