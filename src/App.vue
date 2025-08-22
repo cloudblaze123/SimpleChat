db680b076c3ccbda36bb99db8fe984f82ee4a246
cloudblaze123 <cloudblaze123@gmail.com>
2025-08-22 22:44:41 +0800

    小更新：安装tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <p class="text-green-500">hi tailwindcss</p>
 </template>
 
 <style scoped></style>
